8946caa6697298acd1c0ee6b66dac728bb0c354e
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2022-04-01 22:44:24 +0200

    fix name in mobile app bar

--- src/pages/MobileAppBarProjectPage.js
+++ src/pages/MobileAppBarProjectPage.js
@@ -14,6 +14,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Box from "@material-ui/core/Box";
 import Drawer from "@material-ui/core/Drawer";
+import MainConfig from '../../../projectsResource/main_config';
 
 const useStyles = makeStyles({
     list: {
@@ -25,6 +26,7 @@ const useStyles = makeStyles({
 });
 
 function MobileAppBarProjectPage() {
+    const mainConfig = MainConfig
     const classes = useStyles();
     const [state, setState] = React.useState({
         top: false,
@@ -138,7 +140,7 @@ function MobileAppBarProjectPage() {
                         let url = window.location.protocol + "//" + window.location.host;
                         window.location.replace(url);
                     }}>
-                        Hasan Mhd Amin
+                        {mainConfig.name}
                     </Box>
 
                 </Toolbar>
